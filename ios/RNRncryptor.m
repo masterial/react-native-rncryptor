e22b597bb52ea64512abdcfcb7e60a801109fbe1
masterial <konstantin.k.yurchenko@gmail.com>
2021-04-05 15:22:12 -0700

    Adds more methods.

--- ios/RNRncryptor.m
+++ ios/RNRncryptor.m
@@ -155,6 +155,122 @@ RCT_EXPORT_METHOD(readEncryptedFile:(NSString *)filepath
     resolve(b64);
 }
 
+
+- (void)saveData:(NSData *)data atPath:(NSString*)path
+{
+    NSFileHandle *fileHandler = [NSFileHandle fileHandleForWritingAtPath:path];
+    if(fileHandler == nil) {
+        [[NSFileManager defaultManager] createFileAtPath:path contents:nil attributes:nil];
+        fileHandler = [NSFileHandle fileHandleForWritingAtPath:path];
+    } else {
+        [fileHandler seekToEndOfFile];
+    }
+
+    [fileHandler writeData:data];
+    [fileHandler closeFile];
+}
+
+RCT_EXPORT_METHOD(decryptFileAndSave:(NSString *)filepath
+                  password:(NSString *)password
+                  extension:(NSString *)extension
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+    BOOL fileExists = [[NSFileManager defaultManager] fileExistsAtPath:filepath];
+
+    if (!fileExists) {
+        return reject(@"ENOENT", [NSString stringWithFormat:@"ENOENT: no such file or directory, open '%@'", filepath], nil);
+    }
+
+    NSError *error = nil;
+
+    NSDictionary *attributes = [[NSFileManager defaultManager] attributesOfItemAtPath:filepath error:&error];
+
+    if (error) {
+        reject(@"Error", @"Decrypt failed", error);
+    }
+
+    if ([attributes objectForKey:NSFileType] == NSFileTypeDirectory) {
+        return reject(@"EISDIR", @"EISDIR: illegal operation on a directory, read", nil);
+    }
+
+    NSData *content = [[NSFileManager defaultManager] contentsAtPath:filepath];
+    NSData *decryptedData = [RNDecryptor decryptData:content
+                                        withPassword:password
+                                               error:&error];
+    
+    //    String newpath = filepath+"_decrypted";
+    //    if(extension != null && !extension.isEmpty()) {
+    //      newpath += "." + extension;
+    //    }
+    //    OutputStream outputStream = getOutputStream(newpath, false);
+    //    outputStream.write(bytes);
+    //    outputStream.close();
+    
+    filepath = [NSString stringWithFormat: @"%@_decrypted", filepath];
+    
+    if ([extension length] != 0) {
+        filepath = [NSString stringWithFormat: @"%@.%@", filepath, extension];
+    }
+    
+    [self saveData:decryptedData atPath: filepath];
+        
+    resolve(filepath);
+}
+
+RCT_EXPORT_METHOD(decryptFileAndSaveReturningContent:(NSString *)filepath
+                  password:(NSString *)password
+                  extension:(NSString *)extension
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+    BOOL fileExists = [[NSFileManager defaultManager] fileExistsAtPath:filepath];
+
+    if (!fileExists) {
+        return reject(@"ENOENT", [NSString stringWithFormat:@"ENOENT: no such file or directory, open '%@'", filepath], nil);
+    }
+
+    NSError *error = nil;
+
+    NSDictionary *attributes = [[NSFileManager defaultManager] attributesOfItemAtPath:filepath error:&error];
+
+    if (error) {
+        reject(@"Error", @"Decrypt failed", error);
+    }
+
+    if ([attributes objectForKey:NSFileType] == NSFileTypeDirectory) {
+        return reject(@"EISDIR", @"EISDIR: illegal operation on a directory, read", nil);
+    }
+
+    NSData *content = [[NSFileManager defaultManager] contentsAtPath:filepath];
+    NSData *decryptedData = [RNDecryptor decryptData:content
+                                        withPassword:password
+                                               error:&error];
+    
+    NSString *b64 = [decryptedData base64EncodedStringWithOptions:NSDataBase64EncodingEndLineWithLineFeed];
+
+    //    String newpath = filepath+"_decrypted";
+    //    if(extension != null && !extension.isEmpty()) {
+    //      newpath += "." + extension;
+    //    }
+    //    OutputStream outputStream = getOutputStream(newpath, false);
+    //    outputStream.write(bytes);
+    //    outputStream.close();
+    
+    filepath = [NSString stringWithFormat: @"%@_decrypted", filepath];
+    
+    if ([extension length] != 0) {
+        filepath = [NSString stringWithFormat: @"%@.%@", filepath, extension];
+    }
+    
+    [self saveData:decryptedData atPath: filepath];
+    
+    resolve(b64);
+}
+
+
+
+
 RCT_EXPORT_METHOD(readFile:(NSString *)filepath
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
